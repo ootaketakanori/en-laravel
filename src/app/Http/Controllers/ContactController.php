2ce1610287d324780f3a8022e62625676869c1d3
ootake takanori <mine19876293@gmail.com>
2023-11-27 11:35:33 +0900

    0-1

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+    public function confirm()
+    {
+        return view('confirm');
+    }
+    public function store()
+    {
+        return view('store');
+    }
+}
---
+++ src/database/migrations/2023_11_25_172034_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->integer('tell');
+            $table->text('content');
+            $table->timestamp('create_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
